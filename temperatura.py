a43628fe906c2e3692a446227a1672ad87eae3e8
gagome <danielbarragan@colegioguanenta.edu.co>
2022-04-21 17:56:01 -0500

    Desarrollo: Creación diagrama de flujo

---
+++ temperatura.py
@@ -0,0 +1,13 @@
+"""Programa No. 3: convertidor de celcius a kelvin y farenheit"""
+
+# input
+c =int (input("digite el valor de c: "))
+
+# processing
+
+k =c+273.15
+f =1.8*c+32
+
+#output
+
+input(str(c) + " grados celcius, equivalen a " + str(k) + " grados kelvin y a " + str(f) + " grados farenheit ")
